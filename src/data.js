dac77be982f6023eade7b1d18242cba272b6ae65
Pawan <pawan72891@gmail.com>
2019-07-12 01:12:12 +0530

    data change

--- src/data.js
+++ src/data.js
@@ -11,7 +11,8 @@ export const storeProducts = [
     price: 10,
     company: "GOOGLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "12.3MP f/2.0 primary camera with 1.55 micrometre large pixels, phase detection autofocus (PDAF), laser detection autofocus (LDAF) ) and 8MP front facing camera with f/2.4 aperture 12.7 centimeters (5-inch) Android v7.1 Nougat operating system with 2.15GHz + 1.6GHz 64-bit Qualcomm Snapdragon 821 quad core processor, Adreno 530 GPU, 4GB RAM, 32GB internal memory",
+       
     inCart: false,
     count: 0,
     total: 0
@@ -23,7 +24,8 @@ export const storeProducts = [
     price: 16,
     company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "5.1 inch (12.9 cm) Quad HD 2560 x 1440 pixels with 557 ppi pixel density & 16M colors; Water and dust resistant 12MP f/1.7 primary camera with dual pixel, auto focus, hyperlapse, slow motion and Pro-mode; CMOS 5MP front camera with f/1.7 aperture and selfie flash 4GB RAM, 32GB internal memory expandable up to 200GB",
+      
     inCart: false,
     count: 0,
     total: 0
@@ -35,7 +37,7 @@ export const storeProducts = [
     price: 8,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "20MP primary camera with f/2.2, 28mm, laser auto focus, dual-LED (dual tone) flash, touch focus, face detection, auto-HDR, panorama and 13MP front facing camera Android v6.0 Marshmallow operating system with 1.8GHz + 1GHz Cortex-A53 Mediatek MT6755 Helio P10 octa core processor, Mali-T860MP2 GPU, 4GB RAM, 64GB internal memory expandable up to 256GB and dual SIM dual-standby (4G+4G)",
     inCart: false,
     count: 0,
     total: 0
@@ -47,7 +49,8 @@ export const storeProducts = [
     price: 18,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+     "20MP primary camera with f/2.2, 28mm, laser auto focus, dual-LED (dual tone) flash, touch focus, face detection, auto-HDR, panorama and 13MP front facing camera Android v6.0 Marshmallow operating system with 1.8GHz + 1GHz Cortex-A53 Mediatek MT6755 Helio P10 octa core processor, Mali-T860MP2 GPU, 4GB RAM, 64GB internal memory expandable up to 256GB and dual SIM dual-standby (4G+4G)",
+      
     inCart: false,
     count: 0,
     total: 0
@@ -59,7 +62,10 @@ export const storeProducts = [
     price: 24,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      " Camera: 13+2 MP Dual rear camera | 8 MP front camera Display: 15.24 centimetres (6-inch) HD+ capacitive touchscreen with 720x1440 pixels, 268 ppi pixel density, 16.7M color support and 18:9 aspect ratio Memory, Storage & SIM: 3GB RAM 32GB storage expandable up to 400GB | Dual SIM with dual-standby (4G+4G)",
+       
+      
+      
     inCart: false,
     count: 0,
     total: 0
@@ -71,7 +77,8 @@ export const storeProducts = [
     price: 17,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4.7-inch Retina HD display 12MP camera and 4K video  5MP facetime HD camera with retina flash Touch ID for secure authentication A9 chip iOS 12 with Group facetime, screen time and even faster performance4",
+      
     inCart: false,
     count: 0,
     total: 0
@@ -83,7 +90,7 @@ export const storeProducts = [
     price: 30,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "12mp primary camera with optical image stabilisation, quad-led true tone flash and live photos, 4k video recording at 30 fps and slow-motion video recording in 1080p at 120 fps, 7mp front facing facetime hd camera with retina flash Ios 10 and icloud operating system with a10 fusion chip with integrated m10 motion coprocessor, 32gb internal memory and single nano-sim",
     inCart: false,
     count: 0,
     total: 0
@@ -95,7 +102,7 @@ export const storeProducts = [
     price: 2,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "4.7-inch Retina HD display 12MP camera and 4K video  5MP facetime HD camera with retina flash Touch ID for secure authentication A9 chip iOS 12 with Group facetime, screen time and even faster performance4",
     inCart: false,
     count: 0,
     total: 0
